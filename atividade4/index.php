12d0e1701d5fca42bbb6326723b2c74feeaaf33f
Jonas <joseaugustormjr@gmail.com>
2025-05-21 14:49:52 -0300

    Update index.php

--- atividade4/index.php
+++ atividade4/index.php
@@ -9,7 +9,7 @@
     <form action="dados.php" method="POST">
 
         <label for="senha">Senha:</label> 
-        <input type="text" name="senha" required>
+        <input type="password" name="senha" required>
 
     <br>
     
