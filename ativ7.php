ef060a5cd6eab6818307b58c497dcf5fe05ed499
Jonas <joseaugustormjr@gmail.com>
2025-05-21 16:00:54 -0300

    updated files finally

---
+++ ativ7.php
@@ -0,0 +1,12 @@
+<?php
+
+$dados = array(
+    "nome" => "Alberto Balsalm",
+    "idade" => 32,
+    "cidade" => "CP"
+);
+
+echo "Nome: " . $dados["nome"] . "<br>";
+echo "Idade: " . $dados["idade"] . "<br>";
+echo "Cidade: " . $dados["cidade"] . "<br>";
+?>
